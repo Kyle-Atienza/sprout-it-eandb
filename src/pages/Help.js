5c49272215a38745a8bd5ac82bd579a49c1a5c05
Kyle-Atienza <atienzakylefrancis@gmail.com>
2023-06-04 16:52:17 +0800

    update faq for colors explanation

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -15,28 +15,62 @@ export const Help = () => {
 
   return (
     <>
-      <div className='flex flex-row'>
-        <div className='w-0 lg:w-1/6'>
+      <div className="flex flex-row">
+        <div className="w-0 lg:w-1/6">
           <SideNavBar />
         </div>
 
-        <div className='flex flex-col w-full lg:w-5/6' id='top'>
-          <div className='w-full'>
-            <TopNavBar pageName='Help & FAQs' />
+        <div className="flex flex-col w-full lg:w-5/6" id="top">
+          <div className="w-full">
+            <TopNavBar pageName="Help & FAQs" />
           </div>
 
-          <div className='w-full my-4 py-4 px-4 md:px-6 lg:px-9'>
-            <h1 className='poppins-heading-4 text-primary-600 text-center my-8'>
+          <div className="w-full px-4 py-4 my-4 md:px-6 lg:px-9">
+            <h1 className="my-8 text-center poppins-heading-4 text-primary-600">
               Frequently Asked Questions
             </h1>
-            <div className='full flex flex-col items-center'>
-              <div className='w-full lg:w-3/4'>
+            <div className="flex flex-col items-center full">
+              <div className="w-full lg:w-3/4">
                 <div>
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
+                            What does the colors in the production card
+                            indicate?
+                          </h2>
+
+                          <UpOutlined
+                            className={`${
+                              open ? "" : "rotate-180 transform"
+                            } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
+                          />
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
+                          The use of color assists in illustrating the duration,
+                          measured in weeks, of a production batch in a specific
+                          phase.
+                          <br />
+                          <br />
+                          Green indicates 0 days - 6 days
+                          <br />
+                          Yellow Green indicates 7 days - 13 days
+                          <br />
+                          Yellow indicates 14 days - 20 days
+                          <br />
+                          Orange 21 days - 27 days <br />
+                          Red 28 days and so on
+                          <br />
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                  <Disclosure>
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             How do I start a new production?
                           </h2>
 
@@ -46,7 +80,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           You can start a new production batch by clicking the
                           "Start a new batch" button on the right side of the
                           top navigation bar. A modal will then appear where you
@@ -62,8 +96,8 @@ export const Help = () => {
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             How do I move from one production phase to the next?
                           </h2>
 
@@ -73,7 +107,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           If you think that the current phase of the production
                           batch is now over, you can complete the current phase
                           and proceed to the next one by opening the production
@@ -91,8 +125,8 @@ export const Help = () => {
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             A batch is a the end of the production phase. How do
                             I finish the production process?
                           </h2>
@@ -103,7 +137,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           If a production batch is now at the Post-Production
                           phase, just click the production batch and click "End
                           Production". ONCE YOU PROCEED END PRODUCTION, YOU
@@ -113,38 +147,14 @@ export const Help = () => {
                       </>
                     )}
                   </Disclosure>
-                  {/* <Disclosure>
-                    {({ open }) => (
-                      <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
-                            How do I stop a production batch?
-                          </h2>
-
-                          <UpOutlined
-                            className={`${
-                              open ? "" : "rotate-180 transform"
-                            } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
-                          />
-                        </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat.
-                        </Disclosure.Panel>
-                      </>
-                    )}
-                  </Disclosure> */}
                 </div>
 
-                <div className='mt-14'>
+                <div className="mt-14">
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             There's a new worker in the farm. How can he access
                             Sprout It?
                           </h2>
@@ -155,7 +165,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           To invite a new user, go to Settings tab and type in
                           the details of the user you want to invite, the
                           invited user will then be sent with an invite and the
@@ -168,8 +178,8 @@ export const Help = () => {
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             What's the difference between the 'Owner' and
                             'Worker' user roles?
                           </h2>
@@ -180,7 +190,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           There are two user roles when using Sprout It, the
                           Owner and Worker. Only a user with Owner role can
                           invite another user and grant him/her an Owner or
@@ -192,8 +202,8 @@ export const Help = () => {
                   <Disclosure>
                     {({ open }) => (
                       <>
-                        <Disclosure.Button className='flex mt-4 w-full transition-all justify-between rounded-lg bg-secondary-100 p-4 text-left text-sm font-medium text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75'>
-                          <h2 className='open-heading-6'>
+                        <Disclosure.Button className="flex justify-between w-full p-4 mt-4 text-sm font-medium text-left transition-all rounded-lg bg-secondary-100 text-secondary-600 hover:bg-secondary-200 focus:outline-none focus-visible:ring focus-visible:ring-accent-400 focus-visible:ring-opacity-75">
+                          <h2 className="open-heading-6">
                             I forgot my password. How can I reset it?
                           </h2>
 
@@ -203,7 +213,7 @@ export const Help = () => {
                             } h-5 w-5 text-secondary-600 text-xl leading-none flex justify-center items-center`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel className='p-4 transition-all open-paragraph-sm text-dark-700 bg-light-100 shadow rounded-lg'>
+                        <Disclosure.Panel className="p-4 transition-all rounded-lg shadow open-paragraph-sm text-dark-700 bg-light-100">
                           If you forgot your password, you can easily reset it
                           in the Login page by clicking the "Forgot Password"
                           hyperlink. Type in your email to re-generate the link
